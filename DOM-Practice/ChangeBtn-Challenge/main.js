cd8db91f9f17821421751ff9c6130eebe25aa3c1
bienvenudev <cbienvenu007@gmail.com>
2024-12-15 18:20:41 +0200

    delegate click listener to body

--- DOM-Practice/ChangeBtn-Challenge/main.js
+++ DOM-Practice/ChangeBtn-Challenge/main.js
@@ -1,23 +1,16 @@
 // Button to Change Text and Remove It
 // ●Instructions: Create a button that, when clicked, changes the text of a specified element to "Hello, World!" and another button that removes the text from the element when clicked.
 
-// 2 buttons
-// 1 btn changes to 'Hello World'
-// 2nd btn removes the text
-// an element = 'Bye!'
-
 const body = document.querySelector("body");
 const changeBtn = document.querySelector("#changeBtn");
 const removeBtn = document.querySelector("#removeBtn");
 const element = document.querySelector("h1");
 
 body.addEventListener("click", (e) => {
-  console.log(e.target.attributes.id);
+  const id = e.target.id;
+  if (id == "changeBtn") {
+    element.textContent = "Hello World!";
+  } else if (id == "removeBtn") {
+    element.textContent = "";
+  }
 });
-
-// changeBtn.addEventListener(
-//   "click",
-//   () => (element.textContent = "Hello World!")
-// );
-
-// removeBtn.addEventListener("click", () => (element.textContent = ""));
